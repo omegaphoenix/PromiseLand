148a77e540063083947dbed7a1a562c18a094450
Justin Leong <justinleong16@gmail.com>
2016-06-14 17:58:49 -0700

    mapLimit isn't working

--- async-mapLimit.js
+++ async-mapLimit.js
@@ -1,17 +1,32 @@
-export default function myasyncmap(coll, iteratee, callback) {
+export default function myasyncmapLimit(coll, lim, iteratee, callback) {
   var results = [];
-  for (let i = 0; i < coll.length; i++) {
-    iteratee(coll[i], function(err, transformed) {
-      if (err === null) {
-        results[i] = transformed;
-        if (i == coll.length - 1) {
-          callback(null, results);
-        }
-      }
-      else {
-        return callback(err, results);
-      }
-    });
+  let i = 0;
+  let num_running = 0;
+  while (i < coll.length) {
+      incrementCounter(num_running, lim, function (counter) {
+        num_running = counter;
+        iteratee(coll[i], function(err, transformed) {
+          if (err === null) {
+            results[i] = transformed;
+            if (i == coll.length - 1) {
+              callback(null, results);
+            }
+            i++;
+            num_running--;
+          }
+          else {
+            return callback(err, results);
+          }
+        });
+      });
   }
 }
 
+
+function incrementCounter(counter, lim, callback) {
+  if (counter < lim) {
+    counter++;
+    callback(counter);
+    console.log("counter = " + counter);
+  }
+}
